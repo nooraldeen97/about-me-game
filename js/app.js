94d11374dab2acb8f19f3cc916e172263c973ec8
nooraldeen97 <noor-aldeen97@hotmail.com>
2021-03-29 15:05:48 +0300

    i made a aboutme game repo

---
+++ js/app.js
@@ -0,0 +1,72 @@
+'use strict';
+let UserName = prompt('what\'s your name?')
+
+alert("hello "+UserName+" welcome to my website")
+
+
+
+let ginder = prompt("am I a male ?")
+
+if (ginder.toLowerCase()==='yes' || ginder.toLowerCase()=== 'y' ) {
+    // console.log("correct");
+    alert('correct');
+} else if (ginder.toLowerCase()==='no' || ginder.toLowerCase()=== 'n') {
+    // console.log("rong");
+    alert('rong');
+}
+    
+    
+    
+    let home =prompt("was I born in KSA")
+    
+    if (home.toLowerCase()==='yes' || home.toLowerCase()=== 'y' ) {
+        // console.log("correct");
+        alert('rong');
+    } else if (home.toLowerCase()==='no' || home.toLowerCase()=== 'n') {
+        // console.log("rong");
+        alert('correct');
+}
+        
+        
+   let language =prompt("am I good at english?")
+        
+        if (language.toLowerCase()==='yes' || language.toLowerCase()=== 'y' ) {
+            // console.log("correct");
+            alert('correct');
+        } else if (language.toLowerCase()==='no' || language.toLowerCase()=== 'n') {
+            // console.log("rong");
+            alert('rong');
+}
+            
+            
+            
+            
+    let topics =prompt("Do I like the math and pyhiscs?")
+            
+       if (topics.toLowerCase()==='yes' || topics.toLowerCase()=== 'y' ) {
+                // console.log("correct");
+                alert('correct');
+        } else if (topics.toLowerCase()==='no' || topics.toLowerCase()=== 'n') {
+                // console.log("rong");
+                alert('rong');
+}
+                
+                
+  let tech =prompt("Do I like the proramming?")
+
+  if (tech.toLowerCase()==='yes' || tech.toLowerCase()=== 'y' ) {
+    // console.log("correct");
+    alert('correct');
+} else if (tech.toLowerCase()==='no' || tech.toLowerCase()=== 'n') {
+    // console.log("rong");
+    alert('rong');
+}
+
+
+
+alert ('bye bye '+UserName)
+
+
+
+
+
