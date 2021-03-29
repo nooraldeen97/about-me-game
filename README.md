3ac0fff4e10e261b3bc569227d94220f2a98467d
nooraldeen97 <noor-aldeen97@hotmail.com>
2021-03-29 15:34:39 +0300

    i added about-me-game repo

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # about-me-game
+
+## hello there , you have five questions about me , its a game just answer if correct with yes/y or if rong with no/n ,
+## and it will show you if your answer correct or rong after each question : 
+**am I a male ?** <br>
+**was I born in KSA?** <br>
+**am I good at english?** <br >
+**Do I like the math and pyhiscs?** <br>
+**Do I like the proramming?** <br>
--- js/app.js
+++ js/app.js
@@ -1,70 +1,70 @@
 'use strict';
 let UserName = prompt('what\'s your name?')
 
-alert("hello "+UserName+" welcome to my website")
+alert("hello " + UserName + " welcome to my website")
 
 
 
 let ginder = prompt("am I a male ?")
 
-if (ginder.toLowerCase()==='yes' || ginder.toLowerCase()=== 'y' ) {
+if (ginder.toLowerCase() === 'yes' || ginder.toLowerCase() === 'y') {
     // console.log("correct");
     alert('correct');
-} else if (ginder.toLowerCase()==='no' || ginder.toLowerCase()=== 'n') {
-    // console.log("rong");
-    alert('rong');
-}
-    
-    
-    
-    let home =prompt("was I born in KSA")
-    
-    if (home.toLowerCase()==='yes' || home.toLowerCase()=== 'y' ) {
-        // console.log("correct");
-        alert('rong');
-    } else if (home.toLowerCase()==='no' || home.toLowerCase()=== 'n') {
-        // console.log("rong");
-        alert('correct');
-}
-        
-        
-   let language =prompt("am I good at english?")
-        
-        if (language.toLowerCase()==='yes' || language.toLowerCase()=== 'y' ) {
-            // console.log("correct");
-            alert('correct');
-        } else if (language.toLowerCase()==='no' || language.toLowerCase()=== 'n') {
-            // console.log("rong");
-            alert('rong');
-}
-            
-            
-            
-            
-    let topics =prompt("Do I like the math and pyhiscs?")
-            
-       if (topics.toLowerCase()==='yes' || topics.toLowerCase()=== 'y' ) {
-                // console.log("correct");
-                alert('correct');
-        } else if (topics.toLowerCase()==='no' || topics.toLowerCase()=== 'n') {
-                // console.log("rong");
-                alert('rong');
-}
-                
-                
-  let tech =prompt("Do I like the proramming?")
-
-  if (tech.toLowerCase()==='yes' || tech.toLowerCase()=== 'y' ) {
-    // console.log("correct");
-    alert('correct');
-} else if (tech.toLowerCase()==='no' || tech.toLowerCase()=== 'n') {
+} else if (ginder.toLowerCase() === 'no' || ginder.toLowerCase() === 'n') {
     // console.log("rong");
     alert('rong');
 }
 
 
 
-alert ('bye bye '+UserName)
+let home = prompt("was I born in KSA?")
+
+if (home.toLowerCase() === 'yes' || home.toLowerCase() === 'y') {
+    // console.log("correct");
+    alert('rong');
+} else if (home.toLowerCase() === 'no' || home.toLowerCase() === 'n') {
+    // console.log("rong");
+    alert('correct');
+}
+
+
+let language = prompt("am I good at english?")
+
+if (language.toLowerCase() === 'yes' || language.toLowerCase() === 'y') {
+    // console.log("correct");
+    alert('correct');
+} else if (language.toLowerCase() === 'no' || language.toLowerCase() === 'n') {
+    // console.log("rong");
+    alert('rong');
+}
+
+
+
+
+let topics = prompt("Do I like the math and pyhiscs?")
+
+if (topics.toLowerCase() === 'yes' || topics.toLowerCase() === 'y') {
+    // console.log("correct");
+    alert('correct');
+} else if (topics.toLowerCase() === 'no' || topics.toLowerCase() === 'n') {
+    // console.log("rong");
+    alert('rong');
+}
+
+
+let tech = prompt("Do I like the proramming?")
+
+if (tech.toLowerCase() === 'yes' || tech.toLowerCase() === 'y') {
+    // console.log("correct");
+    alert('correct');
+} else if (tech.toLowerCase() === 'no' || tech.toLowerCase() === 'n') {
+    // console.log("rong");
+    alert('rong');
+}
+
+
+
+alert('bye bye ' + UserName)
 
 
 
