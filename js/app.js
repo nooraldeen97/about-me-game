09c3e95db454d40e60196f11bc0730ff3c964757
nooraldeen97 <noor-aldeen97@hotmail.com>
2021-04-03 01:02:04 +0300

    added Css styling

--- js/app.js
+++ js/app.js
@@ -11,11 +11,9 @@ function Q1() {
     let ginder = prompt("am I a male ?")
 
     if (ginder.toLowerCase() === 'yes' || ginder.toLowerCase() === 'y') {
-        // console.log("correct");
         alert('correct');
         score++;
     } else if (ginder.toLowerCase() === 'no' || ginder.toLowerCase() === 'n') {
-        // console.log("rong");
         alert('rong');
     }
 }
@@ -26,10 +24,8 @@ function Q2() {
     let home = prompt("was I born in KSA?")
 
     if (home.toLowerCase() === 'yes' || home.toLowerCase() === 'y') {
-        // console.log("correct");
         alert('rong');
     } else if (home.toLowerCase() === 'no' || home.toLowerCase() === 'n') {
-        // console.log("rong");
         alert('correct');
         score++
     }
@@ -41,11 +37,9 @@ function Q3() {
     let language = prompt("am I good at english?")
 
     if (language.toLowerCase() === 'yes' || language.toLowerCase() === 'y') {
-        // console.log("correct");
         alert('correct');
         score++;
     } else if (language.toLowerCase() === 'no' || language.toLowerCase() === 'n') {
-        // console.log("rong");
         alert('rong');
     }
 
@@ -58,11 +52,9 @@ function Q4() {
     let topics = prompt("Do I like the math and pyhiscs?")
 
     if (topics.toLowerCase() === 'yes' || topics.toLowerCase() === 'y') {
-        // console.log("correct");
         alert('correct');
         score++
     } else if (topics.toLowerCase() === 'no' || topics.toLowerCase() === 'n') {
-        // console.log("rong");
         alert('rong');
     }
 
@@ -72,11 +64,9 @@ function Q5() {
     let tech = prompt("Do I like the proramming?")
 
     if (tech.toLowerCase() === 'yes' || tech.toLowerCase() === 'y') {
-        // console.log("correct");
         alert('correct');
         score++;
     } else if (tech.toLowerCase() === 'no' || tech.toLowerCase() === 'n') {
-        // console.log("rong");
         alert('rong');
     }
 
@@ -92,7 +82,6 @@ function Q6() {
         guessNum = prompt("guess the number which i have in my mind its (1-10) you have four trial to guess ?");
         if (guessNum == num) {
             alert("correct answer");
-            // console.log("correct");
             score++;
             i = 3;
             break;
@@ -100,21 +89,19 @@ function Q6() {
             alert("too low");
         } else if (guessNum > num) {
             alert("too high");
-            // console.log("rong");
         } if (i == 2) {
             alert("the correct answer is 7");
         }
-      
+
     }
 
 }
 function Q7() {
+
     let planet = ['mercury', 'venus', 'earth', 'mars', 'saturn', 'neptune'];
     let pGuess = '';
-    // this loop for the attempt   
 
-    for (let x = 0; x < 6; x++)
-    {
+    for (let x = 0; x < 6; x++) {
         pGuess = pGuess.toLowerCase();
         pGuess = prompt('mention one planet in our galaxy!!');
 
@@ -152,45 +139,5 @@ alert('bye bye ' + UserName);
 
 
 
-// alert('"mention my preferable colors among these three color (red, blue, green) in order, which the most fovairate one take 1 and so on?"');
 
-// let favColor = ['blue', 'green', 'red'];
-// let guessColor = [prompt('first color').toLowerCase(), prompt('second color').toLowerCase(), prompt('third color').toLowerCase()];
 
-// for (let i = 0; i < 5; i++) {
-
-//     if (guessColor[0] == favColor[0] && guessColor[1] == favColor[1] && guessColor[2] == favColor[2]) {
-//         guessColor = alert("correct answer");
-//         // console.log("correct");
-//         score++;
-//         break;
-//     } else if (guessColor[0] !== favColor[0] || guessColor[1] !== favColor[1] || guessColor[2] !== favColor[2]) {
-//         alert("wrong, try again?");
-//         guessColor = [prompt('first color').toLowerCase(), prompt('second color').toLowerCase(), prompt('third color').toLowerCase()];
-//         // console.log("rong");
-//         if (i == 4)
-//             alert("the correct answer is (blue , green , red)");
-//     }
-
-// }
-
-// let GName=prompt('is my name nooraldeen?');
-// GName=GName.toLowerCase();
-// for (let x = 0; x < 4; x++) {
-//     switch (GName) {
-//        case 'y' || 'yes' :
-//            alert('correct');
-//            x=4;
-//            break;
-//            case 'n' || 'no' :
-//                alert ('wrong amswer');
-//               GName= prompt('try again');
-//              break;
-    
-//        // default:
-//        //     alert('by the way my name is nooraldeen');
-//        //     break;
-//     }
-    
-    
-// }
